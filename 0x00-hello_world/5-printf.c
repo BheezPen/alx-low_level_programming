918aadf12c0f0cde74cf54bb4ee2edd6414f238c
BheezPen <habeeblahaberejo01@gmail.com>
2022-06-02 21:31:18 -0700

    Task 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,5 @@
-gcc $CFILE
 #include <stdio.h>
 int main(void)
-{
-printf("with proper grammar, but the outcome is a piece of art,\n");
+{printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
 }
